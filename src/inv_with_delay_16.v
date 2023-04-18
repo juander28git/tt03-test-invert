ff65ebfd5b1270e7bd8f6756ca5b371b2d8c6605
Juander28 <85529742+Juander28@users.noreply.github.com>
2023-04-18 08:57:24 -0500

    Create inv_with_delay_16.v

---
+++ src/inv_with_delay_16.v
@@ -0,0 +1,8 @@
+module inv_with_delay_16(input A,output Y);
+  `ifdef COCOTB_SIM
+  assign #0.02 Y = ~A; // pick a fairly quick delay from the tt_025C_1v80 liberty file
+                       // the actualy delay per stage is going to be slower
+  `else
+  sky130_fd_sc_hd__inv_16 inv(.A(A),.Y(Y));
+  `endif
+endmodule
