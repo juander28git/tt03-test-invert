3fef0484fec785556096779a58dc189afdb49f4f
juander28git <127985526+juander28git@users.noreply.github.com>
2023-04-18 10:40:47 -0500

    Update inv_with_delay.v

--- src/inv_with_delay.v
+++ src/inv_with_delay.v
@@ -3,6 +3,6 @@ module inv_with_delay(input A,output Y);
   assign #0.02 Y = ~A; // pick a fairly quick delay from the tt_025C_1v80 liberty file
                        // the actualy delay per stage is going to be slower
   `else
-  sky130_fd_sc_hd__inv_2 inv(.A(A),.Y(Y));
+  sky130_fd_sc_hd__inv_1 inv(.A(A),.Y(Y));
   `endif
 endmodule
