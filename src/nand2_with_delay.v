00de5eff6a6af5dec4abf28f84609bd8623f766c
Juander28 <85529742+Juander28@users.noreply.github.com>
2023-04-18 09:38:42 -0500

    Update nand2_with_delay.v

--- src/nand2_with_delay.v
+++ src/nand2_with_delay.v
@@ -2,6 +2,6 @@ module nand2_with_delay(input A,input B,output Y);
   `ifdef COCOTB_SIM
   assign #0.05 Y = ~(A & B);
   `else
-  sky130_fd_sc_hd__nand2_2 nand2(.A(A),.B(B),.Y(Y));
+  sky130_fd_sc_hd__nand2_8 nand2(.A(A),.B(B),.Y(Y));
   `endif
 endmodule
